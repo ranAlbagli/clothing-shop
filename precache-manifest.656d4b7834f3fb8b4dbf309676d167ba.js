e0fbd401f2c6fab3237b84f85ce38d78eecd723d
Ran  albagli <albagli.ran@gmail.com>
2019-08-28 13:59:53 +0300

    Updates

---
+++ precache-manifest.656d4b7834f3fb8b4dbf309676d167ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a3a61ab490caefbefae46b3dd4a92a",
+    "url": "/clothing-shop/index.html"
+  },
+  {
+    "revision": "33ee30344e8c802727fd",
+    "url": "/clothing-shop/static/css/main.a8939dee.chunk.css"
+  },
+  {
+    "revision": "d10780e8c3df7c59e68f",
+    "url": "/clothing-shop/static/js/2.98001ad9.chunk.js"
+  },
+  {
+    "revision": "33ee30344e8c802727fd",
+    "url": "/clothing-shop/static/js/main.ae3d61df.chunk.js"
+  },
+  {
+    "revision": "6cbad4574bd2be90bb76",
+    "url": "/clothing-shop/static/js/runtime~main.c35d68a6.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/clothing-shop/static/media/crown.19ef7b17.svg"
+  }
+]);
